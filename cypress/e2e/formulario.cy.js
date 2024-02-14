21bea8914b8b3912b642750401250eed5dca2e37
Najara Guiland Ferraz <93666690+NajaraGF@users.noreply.github.com>
2024-02-14 17:53:07 -0300

    Movendo arquivos

---
+++ cypress/e2e/formulario.cy.js
@@ -0,0 +1,5 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('file:///C:/Users/najag.LAPTOP-4L06GP1P/AppData/Local/Temp/fa350bbc-de9d-4285-b628-59de5ebe741d_formulario-teste.zip.41d/formulario-teste/index.html')
+  })
+})
